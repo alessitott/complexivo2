4f30ed54438c2a8877d830354aa58055d69379f5
alessitott <david098359@gmail.com>
2026-02-27 14:41:12 -0500

    Corrections

--- cine/serializers.py
+++ cine/serializers.py
@@ -4,11 +4,11 @@ from .models import Show, Reservation
 class ShowSerializer(serializers.ModelSerializer):
     class Meta:
         model = Show
-        fields = ["id", "movie_title", "room", "price", "available_seats"]
+        fields = "__all__"
 
 class ReservationSerializer(serializers.ModelSerializer):
     showid_nombre = serializers.CharField(source="show_id.movie_title", read_only=True)
 
     class Meta:
         model = Reservation
-        fields = ["id", "show_id","showid_nombre", "customer_name", "seats", "status", "created_at"]
+        fields = "__all__, showid_nombre"
